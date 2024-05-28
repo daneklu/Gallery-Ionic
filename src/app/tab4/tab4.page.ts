7fc1874a3e9d2fcf87c85b7c19100454ac2dea76
daneklu <84471525+daneklu@users.noreply.github.com>
2024-05-28 09:26:21 -0600

    sirve logout

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../authentication.service';
 
 @Component({
-  selector: 'app-tab4',
-  templateUrl: 'tab4.page.html',
-  styleUrls: ['tab4.page.scss']
+ selector: 'app-tab4',
+ templateUrl: 'tab4.page.html',
+ styleUrls: ['tab4.page.scss']
 })
 export class Tab4Page {
-  constructor() {}
+ user: any;
+
+ constructor(
+   public route: Router,
+   public authService: AuthenticationService
+ ) {
+   this.user = authService.getProfike();
+ }
+
+ async logout() {
+   try {
+     await this.authService.singOut();
+     this.route.navigate(['/landing']);
+   } catch (error) {
+     console.log(error);
+   }
+ }
 }
